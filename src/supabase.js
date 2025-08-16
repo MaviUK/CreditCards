6a3c7e2271752f435e6545a2bbd266cd6a18e64c
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-08-16 22:57:17 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,14 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ccuhdzcsfmpfdkegcrpa.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdWhkemNzZm1wZmRrZWdjcnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNTgyODgsImV4cCI6MjA3MDkzNDI4OH0.vVxuX6rkAMPazThP8n5Um8sS09eK-JRQjOYa7zZw1so';
+const { data, error } = await supabase
+  .from('payments')
+  .insert([{
+    from_card: cardId,
+    amount: 100.00,
+    terminal_fee_percent: 1.75,
+    terminal_fee_fixed: 0.30
+  }])
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
