bea397747bf8dbc7ed010a76b01ef56ef46a485e
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-08-16 23:13:28 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://ccuhdzcsfmpfdkegcrpa.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdWhkemNzZm1wZmRrZWdjcnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNTgyODgsImV4cCI6MjA3MDkzNDI4OH0.vVxuX6rkAMPazThP8n5Um8sS09eK-JRQjOYa7zZw1so';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
